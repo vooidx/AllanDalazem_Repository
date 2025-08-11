ef99e7d4b29ae5d481f41034ac1433b7f807be64
voidx <matakillallan13@gmail.com>
2025-08-11 10:23:11 -0300

    Script Allan

---
+++ GitHub_Example/Assets/Scripts/Allan.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Allan : MonoBehaviour
+{
+    private string nome;
+
+    void Start()
+    {
+        nome = "Allan";
+    }
+
+    void Update()
+    {
+        
+    }
+}
